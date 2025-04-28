9c3ffb6c7dd63a734c5fd8534d08047b49146c53
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-29 01:26:13 +0700

    finish

--- app/(tab)/(home)/index.tsx
+++ app/(tab)/(home)/index.tsx
@@ -4,25 +4,36 @@ import { router } from "expo-router";
 import { Button } from "react-native";
 import React from "react";
 import { View } from "react-native";
-
+import FunctionCard from "@/components/ui/functionCard";
+import { QrCodeIcon, SparklesIcon } from "react-native-heroicons/outline";
 export default function Index() {
   return (
     <ThemedView className="flex flex-1 flex-col pt-20 px-4">
-      <View className="flex flex-row mb-8">
-        <ThemedText type="title">Thin</ThemedText>
-        <ThemedText type="title" lightColor="#c1121f" darkColor="#c1121f">
-          Maker
-        </ThemedText>
+      <View className="flex flex-row mb-8 justify-between">
+        <View className="flex flex-row">
+          <ThemedText type="title">Thin</ThemedText>
+          <ThemedText type="title" lightColor="#c1121f" darkColor="#c1121f">
+            Maker
+          </ThemedText>
+        </View>
       </View>
 
-      <Button
-        title="Go to QR Code Generator"
-        onPress={() => router.push("/qrCodeGenerator")}
-      />
-      <Button
-        title="Go to Remove BG"
-        onPress={() => router.push("/removeBg")}
-      />
+      {/* <View className="flex flex-row ">
+        <View className="w-1/2 bg-red-500"></View>
+        <View className="w-1/2 bg-blue-500"></View>
+      </View> */}
+      <View className="flex flex-row gap-5">
+        <FunctionCard
+          icon="qrcode"
+          link="/(home)/removeBg"
+          title="Hello owrld"
+        />
+        <FunctionCard
+          icon="qrcode"
+          link="/(home)/removeBg"
+          title="Hello owrld"
+        />
+      </View>
     </ThemedView>
   );
 }
--- components/ui/functionCard.tsx
+++ components/ui/functionCard.tsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { View } from "react-native";
 import { ThemedText } from "../ThemedText";
-
+import { QrCodeIcon, SparklesIcon } from "react-native-heroicons/outline";
+import { Link } from "expo-router";
 interface FunctionCardProps {
-
+  icon: "qrcode" | "bgRemove";
+  title: string;
+  link: string;
 }
 
-export default function FunctionCard() {
+export default function FunctionCard(props: FunctionCardProps) {
+  const { icon, title, link } = props;
   return (
-    <View className="relative flex flex-col my-6 shadow-sm border border-slate-200 rounded-lg w-96">
-      <View className="p-4">
-        <ThemedText className="mb-2 text-slate-800 text-xl font-semibold">
-          Website Review Check Update from Our Team in San Francisco
-        </ThemedText>
-        <ThemedText className="text-slate-600 leading-normal font-light">
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to Naviglio where you can enjoy the main night life in
-          Barcelona.
-        </ThemedText>
+    <Link href={link as string} style={{ flex: 1 }}>
+      <View className="flex-1 flex-col shadow-sm border border-white rounded-lg w-full">
+        <View className="p-4 flex flex-col items-center justify-center">
+          {/* Dynamically set the icon based on the `icon` prop */}
+          {icon === "qrcode" && <QrCodeIcon size={54} color="white" />}
+          {icon === "bgRemove" && <SparklesIcon size={54} color="white" />}
+
+          <ThemedText className="text-slate-600 leading-normal font-light">
+            {title} {/* Dynamically show the title */}
+          </ThemedText>
+        </View>
       </View>
-    </View>
+    </Link>
   );
 }
