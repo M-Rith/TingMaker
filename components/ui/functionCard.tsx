6cb47d4c287446b621e1f249883c27b623b09a72
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-29 23:22:16 +0700

    fixing bugs

--- components/ui/functionCard.tsx
+++ components/ui/functionCard.tsx
@@ -13,14 +13,13 @@ export default function FunctionCard(props: FunctionCardProps) {
   const { icon, title, link } = props;
   return (
     <Link href={link as string} style={{ flex: 1 }}>
-      <View className="flex-1 flex-col shadow-sm border border-white rounded-lg w-full">
+      <View className="flex-1 shadow-sm border border-white rounded-lg w-full h-48">
         <View className="p-4 flex flex-col items-center justify-center">
-          {/* Dynamically set the icon based on the `icon` prop */}
           {icon === "qrcode" && <QrCodeIcon size={54} color="white" />}
           {icon === "bgRemove" && <SparklesIcon size={54} color="white" />}
 
           <ThemedText className="text-slate-600 leading-normal font-light">
-            {title} {/* Dynamically show the title */}
+            {title}
           </ThemedText>
         </View>
       </View>
